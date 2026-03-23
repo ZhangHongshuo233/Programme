bf1246eeac8dcace8bd72c6e0890042e3fdbf0bb
KleinBlue <3250102354@zju.edu.cn>
2026-03-23 20:02:03 +0800

    chore(auto-sync): snapshot 2026-03-23 20:02:03

--- fds data structures and algorithums/hashtable.h
+++ fds data structures and algorithums/hashtable.h
@@ -1,6 +1,22 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 
+typedef struct data{
+    int val;
+}datatype;
 
+typedef struct{
+    datatype* head;
+    int tlen;
+    int cnt;
+}hs_table;
+
+hs_table* create_table(int tlen);
+int Insert(hs_table* table, int val);
+int Search(hs_table* table, int val);
+int Delete(hs_table* table, int val);
+float LoadFactor(hs_table* table);
+int Resize(hs_table* table, int new_tlen);
+int Destroy(hs_table* table);
 
 #endif // HASHTABLE_H
