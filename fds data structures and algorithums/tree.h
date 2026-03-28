c36d8c44051da40115542c8676b7c35ec5d8c5ee
KleinBlue <3250102354@zju.edu.cn>
2026-03-28 20:57:05 +0800

    chore(auto-sync): snapshot 2026-03-28 20:57:05

--- fds data structures and algorithums/tree.h
+++ fds data structures and algorithums/tree.h
@@ -4,26 +4,45 @@
 typedef struct data{
     int val;
     char name[20];
-}data;
+}datatype;
 
-typedef struct node{
-    data data;
-    struct node *left;
-    struct node *right;
-}node;
+typedef struct TreeNode{
+    datatype data;
+    struct TreeNode *left;
+    struct TreeNode *right;
+}TreeNode;
 
-//stack for tree traversal(栈结点，用于非递归遍历树)
+//stack for tree traversal(栈结点结构，用于非递归遍历树)
 typedef struct stacknode{
-    node* tree_node;
+    TreeNode* tree_node;
     struct stacknode *next;
-}stacknode;
+}StackNode;
 
-typedef struct tree{
-    node *top;
-    int size;
-}tree;
+//栈结构(链式栈)
+typedef struct{
+    StackNode* top;
+    int count;
+}LinkStack;
 
-  
+//栈操作函数声明
+LinkStack* CreateLinkStack();
+int Push(LinkStack* stack, StackNode* node);
+TreeNode* Pop(LinkStack* stack, datatype* data);
+int IsEmpty(LinkStack* stack);
+int ClearStack(LinkStack* stack);
+void DestroyStack(LinkStack* stack);
 
-
+//树操作函数声明
+void CreateRoot(TreeNode** root, datatype* data, int* idx);
+void PreOrderTraversal(TreeNode* tree);
+void InOrderTraversal(TreeNode* tree);
+void PostOrderTraversal(TreeNode* tree);
+void PreOrderTraversalNonRecursive(TreeNode* tree);
+void InOrderTraversalNonRecursive(TreeNode* tree);
+void PostOrderTraversalNonRecursive(TreeNode* tree);
+int GetTreeDepth(TreeNode* tree);
+int GetNodeCount(TreeNode* tree);
+int GetLeafCount(TreeNode* tree);
+int ClearTree(TreeNode** tree);
+void DestroyTree(TreeNode** tree);
 #endif 
