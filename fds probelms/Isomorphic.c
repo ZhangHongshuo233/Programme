4e91bdb86d19f1237d28d320ae834c97ddddaf27
KleinBlue <3250102354@zju.edu.cn>
2026-03-30 09:02:05 +0800

    chore(auto-sync): snapshot 2026-03-30 09:02:05

--- fds probelms/Isomorphic.c
+++ fds probelms/Isomorphic.c
@@ -6,8 +6,17 @@ struct TreeNode {
     Tree  Right;
 };
 #include <stdio.h>
-#
+#include <stdlib.h>
+#include <string.h>
 
 int Isomorphic( Tree T1, Tree T2 ){
-    if(T1 == NULL && )
+    if(T1 == NULL && T2 == NULL){
+        return 1;
+    }else if (T1 == NULL || T2 == NULL){
+        return 0;
+    }else if (T1->Element != T2->Element){
+        return 0;
+    }
+    return (Isomorphic(T1->Left, T2->Left) && Isomorphic(T1->Right, T2->Right)) ||
+           (Isomorphic(T1->Left, T2->Right) && Isomorphic(T1->Right, T2->Left));
 }
