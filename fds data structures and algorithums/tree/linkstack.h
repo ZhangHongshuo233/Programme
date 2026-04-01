c6174a74e99718f6c08b8fa46966f842e2cbdf72
KleinBlue <3250102354@zju.edu.cn>
2026-04-01 19:07:05 +0800

    chore(auto-sync): snapshot 2026-04-01 19:07:05

--- fds data structures and algorithums/tree/linkstack.h
+++ fds data structures and algorithums/tree/linkstack.h
@@ -5,21 +5,21 @@
 
 // stack node for tree traversal
 typedef struct stacknode {
-    TreeNode *tree_node;
-    struct stacknode *next;
+    TreeNode* tree_node;
+    struct stacknode* next;
 } StackNode;
 
 // linked stack for tree traversal
 typedef struct {
-    StackNode *top;
+    StackNode* top;
     int size;
 } LinkStack;
 
 // stack operation declarations
-LinkStack *CreateLinkStack();
-int PushStack(LinkStack *stack, TreeNode *node);
-TreeNode *PopStack(LinkStack *stack);
-int IsEmptyStack(LinkStack *stack);
-void DestroyLinkStack(LinkStack *stack);
+LinkStack* CreateLinkStack();
+int PushStack(LinkStack* stack, TreeNode* node);
+TreeNode* PopStack(LinkStack* stack);
+int IsEmptyStack(LinkStack* stack);
+void DestroyLinkStack(LinkStack* stack);
 
 #endif // _LINKSTACK_H_
