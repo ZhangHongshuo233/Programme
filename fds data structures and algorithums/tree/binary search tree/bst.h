efa58e86fd875e75e8bade6337b786dc90cd3a60
KleinBlue <3250102354@zju.edu.cn>
2026-04-02 10:52:05 +0800

    chore(auto-sync): snapshot 2026-04-02 10:52:04

---
+++ fds data structures and algorithums/tree/binary search tree/bst.h
@@ -0,0 +1,43 @@
+#ifndef BINARY_SEARCH_TREE_H
+#define BINARY_SEARCH_TREE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum KeyType{
+    KEY_INT,
+    KEY_CHAR
+}KeyType;
+
+typedef struct key{
+    KeyType type;
+    union{
+        int intValue;
+        char charValue;
+    }value;
+}Key;
+
+typedef struct Node{
+    Key key;
+    struct Node* left;
+    struct Node* right;
+}Node;
+
+typedef struct BST{
+    Node* root;
+    int size; 
+}BST;
+
+void CreateBST(BST* bst);
+Node* SearchBST(BST* bst, Key key);
+Node* FindMin(Node* node);
+Node* FindMax(Node* node);
+int Insert(BST* bst, Key key);
+int Delete(BST* bst, Key key); 
+void PreOrderTraversal(Node* node);
+void InOrderTraversal(Node* node);
+void DestroyBST(BST* bst);
+
+
+#endif // BINARY_SEARCH_TREE_H
