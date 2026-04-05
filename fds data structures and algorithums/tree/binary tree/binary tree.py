aab2df8c571e09521e130458e77e2f7bade5047d
KleinBlue <3250102354@zju.edu.cn>
2026-04-05 12:07:05 +0800

    chore(auto-sync): snapshot 2026-04-05 12:07:04

--- fds data structures and algorithums/tree/binary tree/binary tree.py
+++ fds data structures and algorithums/tree/binary tree/binary tree.py
@@ -3,3 +3,11 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+def preorder(root):
+    if not root:
+        return []
+    return [root.val] + preorder(root.left) + preorder(root.right)
+ 
+
+
