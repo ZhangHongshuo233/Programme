465b0ae3fb2949bb17be6f22ff8a04fb9266b90b
KleinBlue <3250102354@zju.edu.cn>
2026-04-26 17:17:05 +0800

    chore(auto-sync): snapshot 2026-04-26 17:17:04

---
+++ fds data structures and algorithums/graph/adj_list.h
@@ -0,0 +1,30 @@
+#ifndef ADJ_LIST_H
+#define ADJ_LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// 链表节点：代表一条边
+typedef struct AdjListNode {
+    int dest;
+    struct AdjListNode* next;
+} AdjListNode;
+
+/*顶点表头*/
+typedef struct AdjList {
+    AdjListNode* head;  /*链表头指针，指向第一个邻接节点*/
+}AdjList;
+
+/*图本体*/
+typedef struct Graph {
+    int V;          /*顶点数*/
+    AdjList* array; /*动态数组，存储所有顶点的表头*/
+}Graph;
+
+/*API*/
+Graph* CreateGraph(int V);
+void AddEdge(Graph* graph, int src, int dest);
+void PrintGraph(Graph* graph);
+void DestroyGraph(Graph* graph);
+
+#endif // ADJ_LIST_H
