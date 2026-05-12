046c78e8f0c0efc57d4ae30e945ce32f97f5ae04
KleinBlue <3250102354@zju.edu.cn>
2026-05-12 11:07:03 +0800

    chore(auto-sync): snapshot 2026-05-12 11:07:03

---
+++ fds data structures and algorithums/graph/Edge List/edge_list.h
@@ -0,0 +1,29 @@
+#ifndef EDGE_LIST_H 
+#define EDGE_LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Edge {
+    int src;
+    int dest;
+    int weight;
+}Edge;
+
+/* 定义基于边集数组的图 (专为 Kruskal 算法设计) */
+typedef struct EdgeGraph {
+    int V;          /* 顶点数 */
+    int E;          /* 边数容量 */
+    int edgeCount;  /* 当前实际已添加的边数 */
+    Edge* edges;    /* 存储所有边的动态数组 */
+} EdgeGraph;
+
+/* API 接口声明 */
+EdgeGraph* CreateEdgeGraph(int V, int E);
+void AddEdgeToGraph(EdgeGraph* graph, int src, int dest, int weight);
+void DestroyEdgeGraph(EdgeGraph* graph);
+
+/* Kruskal 算法接口 */
+int KruskalMST(EdgeGraph* graph);
+
+#endif // EDGE_LIST_H
